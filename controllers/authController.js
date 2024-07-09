5a49ad65149ca3e9bb0dab792b95785719332991
MAHESH2056 <maheschy2323@gmail.com>
2024-07-09 23:33:39 +0545

    Create handling Errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,25 @@
 import userModel from "../models/userModel.js";
 
 export const registerController = async (req, res, next) => {
-    try {
-        const { name, email, password } = req.body;
-        //validate
-        if(!name) {
-            next("Name is Required");
-        }
-        if(!email) {
-            next("Email is Required");
-        }
-        if(!password) {
-           next("Password is Required and greater than 6 character");
-        }
-        const existingUser = await userModel.findOne({email});
-        if(existingUser){
-            next("Email Already Register Please Login");
-        }
-        const user = await userModel.create({name, email, password});
-        res.status(201).send({
-            success: true,
-            message: "User Created Successfully",
-            user,
-        });
-    } catch (error) {
-        next(error);
+    const { name, email, password } = req.body;
+    //validate
+    if(!name) {
+        next("Name is Required");
     }
+    if(!email) {
+        next("Email is Required");
+    }
+    if(!password) {
+       next("Password is Required and greater than 6 character");
+    }
+    const existingUser = await userModel.findOne({email});
+    if(existingUser){
+        next("Email Already Register Please Login");
+    }
+    const user = await userModel.create({name, email, password});
+    res.status(201).send({
+        success: true,
+        message: "User Created Successfully",
+        user,
+    });
 };
