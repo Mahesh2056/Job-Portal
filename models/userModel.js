591474d00cec98b6409b9b185ca165edf952742b
MAHESH2056 <maheschy2323@gmail.com>
2024-07-10 12:02:51 +0545

    Create Login API

--- models/userModel.js
+++ models/userModel.js
@@ -37,6 +37,12 @@ const userSchema = new mongoose.Schema(
     this.password = await bcrypt.hash(this.password, salt);
   });
 
+  //compare password
+  userSchema.methods.comparePassword = async function(userPassword) {
+    const isMatch = await bcrypt.compare(userPassword, this.password);
+    return isMatch;
+  };
+
   //JSON WEBTOKEN
   userSchema.methods.createJWT = function () {
     return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
