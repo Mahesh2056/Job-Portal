7874b5fd69b964fe85a73be70f3df9e25b2b6eac
MAHESH2056 <maheschy2323@gmail.com>
2024-07-11 13:05:53 +0545

    Create User API

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import connectDB from "./config/db.js";
 import testRoutes from "./routes/testRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorMiddelware from "./middelwares/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
 //Dot ENV config
 dotenv.config();
@@ -29,6 +30,7 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
 
 //validation middelware
 app.use(errorMiddelware);
