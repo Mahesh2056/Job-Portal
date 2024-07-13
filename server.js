88d50b435949403fe7dfb69e6ac84221a9e7e417
MAHESH2056 <maheschy2323@gmail.com>
2024-07-13 23:09:36 +0545

    Install express-rate-limit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 //securty packges
 // import helmet from "helmet";
 // import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 // files imports
 import connectDB from "./config/db.js";
 // routes import
@@ -29,6 +30,7 @@ const app = express();
 //middlewares
 // app.use(helmet(``));
 // app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
