dcec7ceb5fc24ee03d8ce0059b12ec52c46e14b5
MAHESH2056 <maheschy2323@gmail.com>
2024-07-13 22:46:48 +0545

    Create Pagination filter

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -46,13 +46,23 @@ export const getAllJobsController = async (req, res, next) => {
     if (sort === "z-a") {
         queryResult = queryResult.sort("-position");
     }
+    //pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    queryResult = queryResult.skip(skip).limit(limit);
+    //jobs count
+    const totalJobs = await jobsModel.countDocuments(queryResult);
+    const numOfPage = Math.ceil(totalJobs / limit);
 
     const jobs = await queryResult;
 
     // const jobs = await jobsModel.find({ createdBy:req.user.userId });
     res.status(200).json({
-        totaJobs: jobs.length,
+        totalJobs,
         jobs,
+        numOfPage,
     });
 };
 
